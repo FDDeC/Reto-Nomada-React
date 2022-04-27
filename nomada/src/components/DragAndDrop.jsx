d746b7f6fd1aa407201faed8c2aaffebd5d8c864
FDDeC <fededido089@gmail.com>
2022-04-27 02:42:07 -0300

    whoisWorking

--- nomada/src/components/DragAndDrop.jsx
+++ nomada/src/components/DragAndDrop.jsx
@@ -1,23 +1,30 @@
 import React from 'react';
-import 'antd/dist/antd.less';
+import 'antd/dist/antd.css';
 import { Upload, message } from 'antd';
 import { InboxOutlined } from '@ant-design/icons';
-
 const { Dragger } = Upload;
+const whoIsUrl = process.env.REACT_APP_API_WHOIS_URL
+const whoIsKey = process.env.REACT_APP_API_WHOIS_KEY
 
 const props = {
   name: 'file',
-  multiple: true,
-  //action: 'https://www.mocky.io/v2/5cc8019d300000980a055e76',
+  multiple: false,
+  accept:".jpg,.png",
+  action: whoIsUrl, 
+  headers: { "nomada": whoIsKey },  
   onChange(info) {
     const { status } = info.file;
     if (status !== 'uploading') {
-      console.log(info.file, info.fileList);
+      //console.log(info.file, info.fileList);      
     }
-    if (status === 'done') {
-      message.success(`${info.file.name} file uploaded successfully.`);
+    if (status === 'done') {        
+      if (info.file.response.error.length) {
+        message.error(`${info.file.response.error}.`);
+      } else {
+        message.success(`Es ${info.file.response.actorName} !!`);
+      }      
     } else if (status === 'error') {
-      message.error(`${info.file.name} file upload failed.`);
+      message.error(`${info.file.name} error de subida.`);
     }
   },
   onDrop(e) {
@@ -31,10 +38,9 @@ export default function DragAndDrop(){
       <p className="ant-upload-drag-icon">
         <InboxOutlined />
       </p>
-      <p className="ant-upload-text">Click or drag file to this area to upload</p>
+      <p className="ant-upload-text">Selecciona o arrastra una foto</p>
       <p className="ant-upload-hint">
-        Support for a single or bulk upload. Strictly prohibit from uploading company data or other
-        band files
+        En caso detectemos una persona del cine en tu foto te diremos quién es y dónde trabajó
       </p>
     </Dragger>
   )
