0239b13be424948fe27ecd6b41ac62234a2b55ea
FDDeC <fededido089@gmail.com>
2022-04-30 13:12:37 -0300

    restart button &  voteIcon

--- nomada/src/components/ActorDetails.jsx
+++ nomada/src/components/ActorDetails.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect } from "react";
-import { Row, Col, Skeleton, List, Image, Tag, Button } from "antd";
-import { ArrowLeftOutlined } from '@ant-design/icons';
+import { Row, Col, List, Image, Tag, Button } from "antd";
+import { ArrowLeftOutlined, StarFilled } from '@ant-design/icons';
 import { useSelector, useDispatch } from "react-redux";
 import { useParams, useNavigate, Link } from "react-router-dom";
 import { setActorDetails } from "../features/whoIs/whoIsSlice";
@@ -31,37 +31,33 @@ function ActorDetails() {
   }
 
   return (
-    <>
-      <Col style={{ padding: "5px", textAlign: "center" }} xs={10} sm={8} md={6}><Link to="/">
-        <Button style={{width:"100%", minWidth:"100px", maxWidth:"230px"}} type="primary" icon={<ArrowLeftOutlined />}>
-          Regresar
-        </Button>
-      </Link></Col>  
-             
-      
+    
     <Row style={{backgroundColor:"#fafafa"}}>
       <Col xs={24} sm={8} md={6}>
         <Row>
           <Col style={{display:"flex", justifyContent:"center", padding:"5px", height:"100%"}} xs={10} sm={24}>
             <Image
-              style={{
-                minWidth: "100px",
-                maxHeight: "350px",                
-          minHeight:"150px"}}
+              style={{               
+                maxHeight: "40vh"
+                }}
               src={`${movieDbImg}w500${details.profile_path}`}
             />
           </Col>
           <Col style={{ display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center", gap: 10, padding:"5px"}} xs={14} sm={24}>
             <h3>{details.name}</h3>
             <Tag color="gold">{details.gender === 2 ? "Masculino" : "Femenino"}</Tag>
-            <Tag color="gold"> Popularidad: { details.popularity }</Tag>
+              <Tag color="gold"> Popularidad: {details.popularity}</Tag>
+              <Link to="/">
+        <Button style={{width:"100%", minWidth:"100px", maxWidth:"230px"}} type="primary" icon={<ArrowLeftOutlined />}>
+          Regresar
+        </Button>
+      </Link>
           </Col>
         </Row>
       </Col>
       <Col style={{padding:"5px"}} xs={24} sm={16} md={18}>
         {/* <h3>Películas:</h3> */}
-        <List
-          //loading={initLoading}
+        <List          
           itemLayout="horizontal"
           pagination={{
             onChange: (page) => {
@@ -76,7 +72,9 @@ function ActorDetails() {
                 <List.Item.Meta
                 avatar={
                   <Image
-                    height={150}
+                    style={{
+                      maxHeight: "25vh"
+                    }}                    
                     src={`${movieDbImg}w500${item.poster_path}`}
                   />
                 }
@@ -89,10 +87,10 @@ function ActorDetails() {
                     }}
                   >
                     <h2>{item.original_title}</h2>
-                    <h4>Pt. {item.vote_average}/10</h4>
+                    <h4><StarFilled /> {item.vote_average}/10</h4>
                   </Row>
                 }
-                description={<><p>{item.overview}</p><p><b>Fecha de estreno:{formatDate(item.release_date)}</b></p></>}
+                description={<><p>{item.overview}</p><p><b>Fecha de estreno: {formatDate(item.release_date)}</b></p></>}
                 />
               
             </List.Item>
@@ -100,7 +98,7 @@ function ActorDetails() {
         />
       </Col>
       </Row>
-      </>
+     
   );
 }
 
